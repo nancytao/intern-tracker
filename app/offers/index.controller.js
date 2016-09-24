56f85357a41b66e9fd4c5cbbd2b4a6fdc60b3aa1
nancytao <nancy.tao42@gmail.com>
2016-09-24 17:19:50 -0400

    Added maps and buttons for offer comparison

--- app/offers/index.controller.js
+++ app/offers/index.controller.js
@@ -3,7 +3,39 @@
 
     angular
         .module('app')
-        .controller('Offers.IndexController', Controller);
+        .controller('Offers.IndexController', Controller, ['$scope',
+            function($scope) {
+                $scope.offers = [
+                    {
+                        company: 'Google',
+                        payrate: '30',
+                        location: 'Boston, MA',
+                        replyby: '2016-10-24',
+                        role: 'Project Management Intern'
+                    },
+                    {
+                        company: 'Bloomberg',
+                        payrate: '25',
+                        location: 'New York, NY',
+                        replyby: '2016-10-21',
+                        role: 'Software Engineer Intern'
+                    },
+                    {
+                        company: 'Northrop Grumman',
+                        payrate: '32',
+                        location: 'McLean, VA',
+                        replyby: '2016-09-30',
+                        role: 'Security Intern'
+                    },
+                    {
+                        company: 'ADP',
+                        payrate: '27',
+                        location: 'Alpharetta, GA',
+                        replyby: '2016-10-14',
+                        role: 'Software Engineer Intern'
+                    }
+                ];
+            }]);
 
     function Controller($window, UserService, FlashService) {
         var vm = this;
