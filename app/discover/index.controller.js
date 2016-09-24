1f04463732520ae83f84417291d9f2518eaa27a4
syoung67 <syoung2019@gatech.edu>
2016-09-24 15:11:37 -0400

    discover internships

--- app/discover/index.controller.js
+++ app/discover/index.controller.js
@@ -8,18 +8,66 @@
     function Controller($window, $q, $log, $timeout, UserService, FlashService) {
         var vm = this;
         vm.user = null;
-        vm.saveInternships = saveInternships;
+
+        vm.simulateQuery = false;
+        vm.isDisabled = false;
+
+        vm.querySearch = querySearch;
+        vm.selectedItemChange = selectedItemChange;
+        vm.searchTextChange = searchTextChange;
+
+        vm.internships = loadAll();
+        vm.newInternship = newInternship
 
         initController();
 
         function initController() {
             UserService.GetCurrent().then(function(user) {
                 vm.user = user;
-                user.internships = {};
-                user.internships.test = {};
-                user.internships.test.name = 'Googel';
-                user.internships.test.recruiter = 'Dat Boi';
             });
         }
+
+        function newInternship(internship) {
+            alert("Sorry! You'll need to populate " + internship);
+        }
+
+        function querySearch(query) {
+            var results = query ? vm.internships.filter(createFilterFor(query)) : vm.internships;
+            if (vm.simulateQuery) {
+                deferred = $q.defer();
+                $timeout(function() { deferred.resolve( results ); },
+                    Math.random() * 1000, false);
+                return deferred.promise;
+            } else {
+                return results;
+            }
+        }
+
+        function searchTextChange(text) {
+            //$log.info('Text changed to ' + text);
+        }
+
+        function selectedItemChange(item) {
+            //$log.info('Item changed to ' + JSON.stringify(item));
+        }
+
+        function loadAll() {
+            var allInternships = 'Google, Microsoft, Amazon, Palantir, Fitbit, Dropbox, Quora, Pinterest';
+            return allInternships.split(/, +/g).map( function (internship) {
+                return {
+                    value : internship.toLowerCase(),
+                    display : internship
+                };
+            });
+        }
+
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(internship) {
+                return (internship.value.indexOf(lowercaseQuery) === 0);
+            };
+
+        }
+
     }
 })();
