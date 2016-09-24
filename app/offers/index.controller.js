e5146909e09d64a37143267a52feae3ed821ed30
nancytao <nancy.tao42@gmail.com>
2016-09-24 17:55:22 -0400

    Controller for offers

--- app/offers/index.controller.js
+++ app/offers/index.controller.js
@@ -3,39 +3,7 @@
 
     angular
         .module('app')
-        .controller('Offers.IndexController', Controller, ['$scope',
-            function($scope) {
-                $scope.offers = [
-                    {
-                        company: 'Google',
-                        payrate: '30',
-                        location: 'Boston, MA',
-                        replyby: '2016-10-24',
-                        role: 'Project Management Intern'
-                    },
-                    {
-                        company: 'Bloomberg',
-                        payrate: '25',
-                        location: 'New York, NY',
-                        replyby: '2016-10-21',
-                        role: 'Software Engineer Intern'
-                    },
-                    {
-                        company: 'Northrop Grumman',
-                        payrate: '32',
-                        location: 'McLean, VA',
-                        replyby: '2016-09-30',
-                        role: 'Security Intern'
-                    },
-                    {
-                        company: 'ADP',
-                        payrate: '27',
-                        location: 'Alpharetta, GA',
-                        replyby: '2016-10-14',
-                        role: 'Software Engineer Intern'
-                    }
-                ];
-            }]);
+        .controller('Offers.IndexController', Controller);
 
     function Controller($window, UserService, FlashService) {
         var vm = this;
@@ -44,10 +12,45 @@
         initController();
 
         function initController() {
-            //get current user
             UserService.GetCurrent().then(function(user) {
                 vm.user = user;
+                if (!user.offers) {
+                    user.offers = {};
+                    user.offerID = 0;
+                }
             });
+        }
+
+        function addOffer() {
+            vm.user.offers[vm.user.offerID] = {};
+            vm.user.offers[vm.user.offerID].id = vm.user.offerID;
+            vm.user.offers[vm.user.offerID].company = null;
+            vm.user.offers[vm.user.offerID].role =  null;
+            vm.user.offers[vm.user.offerID].payrate = null;
+            vm.user.offers[vm.user.offerID].location = null;
+            vm.user.offers[vm.user.offerID].replyby = null;
+            vm.user.offerID++;
+        }
+
+        function saveOffers() {
+            UserService.Update(vm.user)
+                .then(function() {
+                    FlashService.Success('Internships updated');
+                })
+                .catch(function (err) {
+                    FlashService.Error(err);
+                });
+        }
+
+        function deleteOffer(offerId) {
+            delete vm.user.offers[offerId];
+            UserService.Update(vm.user)
+                .then(function() {
+                    FlashService.Success('Internship deleted');
+                })
+                .catch(function (err) {
+                    FlashService.Error(err);
+                });
         }
     }
 })();
