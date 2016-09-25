6d8d69c9bef9bb5fc250653c24ce6f3b23fa847d
nancytao <nancy.tao42@gmail.com>
2016-09-25 01:15:54 -0400

    Offers fixed

--- app/offers/index.controller.js
+++ app/offers/index.controller.js
@@ -49,6 +49,7 @@
             var counter = 1;
             var string = 'http://maps.googleapis.com/maps/api/staticmap?size=800x600&maptype=roadmap&key=AIzaSyDodM8wUpuA2CAd6Fa1CEC0Drgv1vxsztM&format=png&visual_refresh=true';
 
+
             for (var key in vm.user.internships) {
                 if (!vm.user.internships.hasOwnProperty(key)) {
                     continue;
@@ -62,8 +63,6 @@
                     }
                 }
             }
-
-
 
             console.log(string);
             return string;
